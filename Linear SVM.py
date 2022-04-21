ca1d16b51f1109cb6592f7fdf31e585270cce3b7
n07jihan <92074361+n07jihan@users.noreply.github.com>
2022-04-21 22:56:58 +0800

    Add files via upload

---
+++ Linear SVM.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import stats
+
+# use seaborn plotting defaults
+import seaborn as sns; sns.set()
+
+data = pd.read_csv('tinggi_badan.csv')
+data.head()
+
+X = data.drop('BB', axis=1)
+y = data['BB']
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20)
+
+from sklearn.svm import SVC
+svclassifier = SVC(kernel='linear')
+svclassifier.fit(X_train, y_train)
+
+y_pred = svclassifier.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(confusion_matrix(y_test,y_pred))
+print(classification_report(y_test,y_pred))
+
+from sklearn.datasets import make_blobs
+X, y = make_blobs(n_samples=50, centers=2,
+                  random_state=0, cluster_std=0.60)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn');
+
+xfit = np.linspace(-1, 3.5)
+plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn')
+plt.plot([0.6], [2.1], 'x', color='red', markeredgewidth=2, markersize=10)
+
+for m, b in [(1, 0.65), (0.5, 1.6), (-0.2, 2.9)]:
+    plt.plot(xfit, m * xfit + b, '-k')
+
+plt.xlim(-1, 3.5);
